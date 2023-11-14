f95c97079e4b434cea588cbd493afce4ca1f3663
nlunce <nluncef@gmail.com>
2023-11-13 20:09:24 -0700

    protected routes

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -31,5 +31,15 @@ export async function action({ request }) {
     throw json({ mesasge: "Could not authenticate user." }, { status: 500 });
   }
 
+  const resData = await response.json();
+  const token = resData.token;
+
+  localStorage.setItem("token", token);
+
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+
+  localStorage.setItem("expiration", expiration.toISOString());
+
   return redirect("/");
 }
